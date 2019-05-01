294e0ba8eebd10bd2eb186802eb461ad8eb21851
Ogutierrez08 <42872009+Ogutierrez08@users.noreply.github.com>
2019-05-01 10:42:52 -0500

    Google API

---
+++ API/bingAPI.js
@@ -0,0 +1,30 @@
+const https = require('https')
+const config = require('../config/bingConfig')
+
+module.exports = {
+  bingWebSearch  (query) {
+    https.get({
+      hostname: 'api.cognitive.microsoft.com',
+      path: '/bing/v7.0/search?q=' + encodeURIComponent(query),
+      headers: { 'Ocp-Apim-Subscription-Key': config.subscriptionKey }
+    }, res => {
+      let body = ''
+      // eslint-disable-next-line no-return-assign
+      res.on('data', part => body += part)
+      res.on('end', () => {
+        for (var header in res.headers) {
+          if (header.startsWith('bingapis-') || header.startsWith('x-msedge-')) {
+            //   console.log(header + ': ' + res.headers[header])
+          }
+        }
+        console.log('BING API SEARCH:')
+        console.dir(`${query}: LA CANTIDAD DE RESULTADOS ENCONTRADOS ES: ` + JSON.parse(body).webPages.totalEstimatedMatches, { colors: false, depth: null })
+      })
+      res.on('error', e => {
+        console.log('Error: ' + e.message)
+        throw e
+      })
+    })
+  }
+
+}
---
+++ API/googleAPI.js
@@ -0,0 +1,16 @@
+const google = require('googleapis').google
+const customsearch = google.customsearch('v1')
+const config = require('../config/googleConfig')
+
+module.exports = {
+  googleSearch (query) {
+    customsearch.cse.list({ cx: config.CX, q: query, auth: config.API_KEY }, function (err, resp) {
+      if (err) {
+        console.log('An error occured', err)
+        return
+      }
+      console.log('Google Search API')
+      console.log(`${query}: LA CANTIDAD DE RESULTADOS ENCONTRADOS ES:` + JSON.stringify(resp.data.queries.request[0].totalResults))
+    })
+  }
+}
